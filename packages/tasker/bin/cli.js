580f9d49135be874f1446baf00a869a7c6176f44
carlos <carlos.pastor@mailonline.co.uk>
2018-10-09 11:10:25 +0200

    feat: 🎸 bind run method with run command

--- packages/tasker/bin/cli.js
+++ packages/tasker/bin/cli.js
@@ -2,15 +2,13 @@
 
 const args = require('args');
 const getConfig = require('../src/getConfig');
+const run = require('../src/run');
 
 const CONFIG_FILE = 'tasker.config.js';
 
 args
   .option('config', `Path to the config file. If omitted it will search up for ${CONFIG_FILE} in the directory tree`, null, getConfig)
-  .command('run', 'execs the specified task.', (name, sub, options) => {
-    // eslint-disable-next-line no-console
-    console.log('#### RUN COMMAND', name, sub, options);
-  }, ['r']);
+  .command('run', 'execs the specified task.', async (name, [task], options) => run(task, await options.config), ['r']);
 
 // eslint-disable-next-line no-unused-vars
 const flags = args.parse(process.argv, {
